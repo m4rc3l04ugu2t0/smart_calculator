42e7b0986f3aa19e5fdb048a6b66f05c5e8b8618
Marcelo <nextlevlcode@gmail.com>
2024-10-19 15:24:32 -0300

    set port

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,26 @@ mod parse_expression;
 mod structs;
 mod web;
 
-use axum::{routing::post, serve, Router};
-use tokio::net::TcpListener;
+use axum::{routing::post, Router};
+use std::{env::var, net::SocketAddr};
 use web::entry_point::calculate;
 
 pub use self::error::{ClientError, Result};
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    dotenv::dotenv().ok();
+    let port = var("PORT")
+        .ok()
+        .and_then(|p| p.parse::<u16>().ok())
+        .unwrap_or(8000);
+
     let app = Router::new().route("/calculate", post(calculate));
 
-    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    serve(listener, app).await.unwrap();
-
+    let addr = SocketAddr::from(([0, 0, 0, 0], port));
+    axum_server::bind(addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
     Ok(())
 }
